6fdc60addcddb24cb878265dfa87ea3639eaf052
Luiz Matheus Teixeira Rodrigues <matheus.3223@gmail.com>
2024-08-05 21:48:54 -0300

    - Inicialização do Projeto da marca Y-Brands; - Testes iniciais;

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav className='menu-principal'>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Se​rvices</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      </nav>
+    </>
+  )
+}
+
+export default App
